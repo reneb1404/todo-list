a184e21e18dda586caae5ff839f8dbc1fa277407
Rene B. <133756710+Krypto1337@users.noreply.github.com>
2025-01-14 15:41:51 +0100

    Use an array for the data and loop trough it instead of rendering every single component

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,29 @@ import { TaskItem } from "@/src/entities/TaskItem";
 import { TaskForm } from "@/src/features/task/TaskForm";
 
 export default function Home() {
+	const testTaskData = [
+		{
+			title: "Task 1",
+			date: "15.01.2025",
+			todo: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+		},
+		{
+			title: "Task 2",
+			date: "15.01.2025",
+			todo: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+		},
+		{
+			title: "Task 3",
+			date: "15.01.2025",
+			todo: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+		},
+		{
+			title: "Task 4",
+			date: "15.02.2025",
+			todo: "Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit...",
+		},
+	];
+
 	return (
 		<>
 			<section className="text-white bg-surface-10 mt-4 w-full flex flex-col items-center">
@@ -10,26 +33,14 @@ export default function Home() {
 			</section>
 			<section className="p-8 mt-16">
 				<div className="flex flex-wrap justify-center mt-10">
-					<TaskItem
-						title="Task 1"
-						date="15.01.2025"
-						todo="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-					/>
-					<TaskItem
-						title="Task 2"
-						date="15.01.2025"
-						todo="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-					/>
-					<TaskItem
-						title="Task 3"
-						date="15.01.2025"
-						todo="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-					/>
-					<TaskItem
-						title="Task 4"
-						date="15.01.2025"
-						todo="Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit..."
-					/>
+					{testTaskData.map((task) => (
+						<TaskItem
+							key={task.title}
+							title={task.title}
+							date={task.date}
+							todo={task.todo}
+						/>
+					))}
 				</div>
 			</section>
 		</>
