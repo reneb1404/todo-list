927e12026b85bb2e265869115cbd87f7ad3cf613
Rene Bucher <133756710+reneb1404@users.noreply.github.com>
2025-01-21 20:32:10 +0000

    Fixed issue where the Footer isnt at the bottom

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
 				className={`${geistSans.variable} ${geistMono.variable} flex flex-col min-h-screen justify-between antialiased`}
 			>
 				<NavBar />
-				{children}
+				<div className="flex-1">{children}</div>
 				<Footer />
 			</body>
 		</html>
